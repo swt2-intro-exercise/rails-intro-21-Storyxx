a7089a075677754d63f0492341a545772bfba2b6
Storyxx <57802448+Storyxx@users.noreply.github.com>
2021-11-07 21:31:08 +0100

    feature: new-author page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  
+  get 'new_author', to: 'authors#new'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
