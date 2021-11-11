427591e720b75391c7f599760af6b484ee4c25f5
Storyxx <57802448+Storyxx@users.noreply.github.com>
2021-11-11 14:51:59 +0100

    feature: detail page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,7 +6,7 @@ class AuthorsController < ApplicationController
     author_params = params.require(:author).permit(:first_name, :last_name, :homepage)
     @author = Author.new(author_params)
     @author.save
-    #redirect_to @author
+    redirect_to @author
   end
 
   def show
