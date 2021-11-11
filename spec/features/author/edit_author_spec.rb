2758a84b36de44cdc5ac17d0249a74d8d075d96e
Storyxx <57802448+Storyxx@users.noreply.github.com>
2021-11-11 16:05:42 +0100

    fix: test the edit page

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -1,31 +1,36 @@
 require 'rails_helper'
 
 describe "Edit author page", type: :feature do
-  it "should exist at 'edit_author_path' and render withour error" do
-    visit edit_author_path
-  end
 
-  it "should have text inputs for an author's first name, last name, and homepage" do
-    visit edit_author_path
-    expect(page).to have_field('author[first_name]')
-    expect(page).to have_field('author[last_name]')
-    expect(page).to have_field('author[homepage]')
-  end
+    author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+    author.save
 
-  it "should contain a form that is fillable and submittable" do
-    visit edit_author_path
-    page.fill_in 'author[first_name]', with: 'Alan'
-    page.fill_in 'author[last_name]', with: 'Turing'
-    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
-    find('input[type="submit"]').click
-  end
+    it "should exist at 'edit_author_path' and render withour error" do
+        visit edit_author_path 1
+    end
 
-  it "should display an error when submitting an incomplete form" do
-    visit edit_author_path
-    page.fill_in 'author[first_name]', with: 'Alan'
-    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
-    find('input[type="submit"]').click
-    expect(page).to have_text('error')
-  end
+    it "should have text inputs for an author's first name, last name, and homepage" do
+        visit edit_author_path 1
+        expect(page).to have_field('author[first_name]')
+        expect(page).to have_field('author[last_name]')
+        expect(page).to have_field('author[homepage]')
+    end
+
+    it "should contain a form that is fillable and submittable" do
+        visit edit_author_path 1
+        page.fill_in 'author[first_name]', with: 'Alan'
+        page.fill_in 'author[last_name]', with: 'Turing'
+        page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+        find('input[type="submit"]').click
+    end
+
+    it "should display an error when submitting an incomplete form" do
+        visit edit_author_path 1
+        page.fill_in 'author[first_name]', with: 'Alan'
+        page.fill_in 'author[last_name]', with: ''
+        page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+        find('input[type="submit"]').click
+        expect(page).to have_text('error')
+    end
 
 end
