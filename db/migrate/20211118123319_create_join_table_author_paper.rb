5524b27b2cd694da30b7971a9126a853ea9d1c35
Storyxx <57802448+Storyxx@users.noreply.github.com>
2021-11-18 14:41:42 +0100

    fix: migrations

--- db/migrate/20211118123319_create_join_table_author_paper.rb
+++ db/migrate/20211118123319_create_join_table_author_paper.rb
@@ -3,8 +3,8 @@ class CreateJoinTableAuthorPaper < ActiveRecord::Migration[6.0]
     create_join_table :authors, :papers do |t|
       t.index [:author_id, :paper_id]
       t.index [:paper_id, :author_id]
-      t.belongs_to :paper
-      t.belongs_to :author
+      #t.belongs_to :paper
+      #t.belongs_to :author
     end
   end
 end
